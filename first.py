a1d442018011e43b37d613214a710f30875c0c09
unknown <danakaposhak@gmail.com>
2019-09-09 15:19:25 -0500

    First edits

--- first.py
+++ first.py
@@ -1,5 +1,15 @@
 1+1
+#%%
 print('Hello Python!') # This line prints a string
 import sys
 print(sys.version)
 type(12.0)
+
+#%%
+album_sales = {"Back in Black":"50","The Bodyguard":"50","Thriller":"65"}
+album_sales
+album_sales["Thriller"]
+album_sales.keys()
+album_sales.values()
+
+#%%
