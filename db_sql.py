7a746b1c26cfe63e8bc1eff0645c029b5f7f6127
unknown <danakaposhak@gmail.com>
2019-09-19 14:22:49 -0500

    some new work

---
+++ db_sql.py
@@ -0,0 +1,8 @@
+#%%
+import ibm_db
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+
+#%%
---
+++ plot.py
@@ -0,0 +1,9 @@
+#%%
+import matplotlib as plt
+import pandas as pd 
+import numpy as np
+
+#%%
+pd.read_csv("C:\\Users\\danak\\Desktop\\Data science\\MMAE 419\\1018.csv")
+
+#%%
